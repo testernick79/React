1502fc91dc620af44c2f0aaa73c592bc2008db61
Nicholas Fonseca <nicholasfonseca@Nicholass-MacBook-Pro.local>
2019-08-07 20:56:06 -0700

    Lesson2HandsOn

--- React-practice/Lesson2HandsOn/src/index.js
+++ React-practice/Lesson2HandsOn/src/index.js
@@ -1,1 +1,56 @@
-alert("it's alive");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const stateInfo = (
+    state, population, capitol,
+    idahoState, idahoPopulation, idahoCapitol,
+    tennesseState, tennessePopulation, tennesseCapitol,
+    maineState, mainePopulation, maineCapitol,
+    wisconsinState, wisconsinPopulation, wisconsinCapitol, 
+
+) => {
+    return (
+        <table>
+            <tbody>
+                <td><strong>{state}</strong></td>
+                <td><strong>{population}</strong></td>
+                <td><strong>{capitol}</strong></td>
+            </tbody>
+
+            <tbody>
+                <td>{idahoState}</td>
+                <td>{idahoPopulation}</td>
+                <td>{idahoCapitol}</td>
+            </tbody>
+
+            <tbody>
+                <td>{tennesseState}</td>
+                <td>{tennessePopulation}</td>
+                <td>{tennesseCapitol}</td>
+            </tbody>
+
+            <tbody> 
+                <td>{maineState}</td>
+                <td>{mainePopulation}</td>
+                <td>{maineCapitol}</td>
+            </tbody>
+
+            <tbody>
+                <td>{wisconsinState}</td>
+                <td>{wisconsinPopulation}</td>
+                <td>{wisconsinCapitol}</td>
+            </tbody>
+        </table>
+    );
+};
+
+ReactDOM.render(
+    stateInfo(
+        '   STATE   ', '    POPULATION      ', '    CAPITOL     ', 
+        'Idaho', '1.719 Million', 'Boise',
+        'Tennesse', '6.651 Million', 'Nashville',
+        'Maine', '1.113 Million', 'Agusta',
+        'Wisconsin', '6.651 Million', 'Madison',
+    ),
+    document.getElementById('root')
+);
